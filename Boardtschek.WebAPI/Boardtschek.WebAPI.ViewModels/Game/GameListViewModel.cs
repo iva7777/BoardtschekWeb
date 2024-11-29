7a239b0cac047927cabb2790d3aa24e2bb53d138
Salian Dzhihanov <saliandzhihanov@gmail.com>
2024-11-29 13:53:16 +0200

    Added GameListViewModel
    We will need this model for listing games in the home page.

---
+++ Boardtschek.WebAPI/Boardtschek.WebAPI.ViewModels/Game/GameListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Boardtschek.WebAPI.ViewModels.Game
+{
+    public class GameListViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+    }
+}
