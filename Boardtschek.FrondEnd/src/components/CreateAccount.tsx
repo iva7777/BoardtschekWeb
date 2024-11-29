7b4eea59556cf2e0ed6e0c8bafa1e238b9803826
Iva <60818270+iva7777@users.noreply.github.com>
2024-11-29 17:09:01 +0200

    FE: Login and Register Pages

---
+++ Boardtschek.FrondEnd/src/components/CreateAccount.tsx
@@ -0,0 +1,112 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const formSchema = z.object({
+    firstName: z.string()
+        .min(2, {
+            message: "First name must be at least 2 characters.",
+        })
+        .max(50, {
+            message: "First name cannot exceed 50 characters.",
+        }),
+    lastName: z.string()
+        .min(2, {
+            message: "Last name must be at least 2 characters.",
+        })
+        .max(50, {
+            message: "Last name cannot exceed 100 characters.",
+        }),
+    email: z.string()
+        .email({
+            message: "Enter a valid email address.",
+        }),
+})
+
+export function CreateAccount() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            firstName: "",
+            lastName: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
+    return (
+        <>
+            <Form {...form}>
+                <div className="flex h-screen">
+                    {/* Left Section: Headings and Form */}
+                    <div className="w-1/2 flex flex-col items-center justify-center bg-gray-100 p-8">
+                        {/* Headings */}
+                        <div className="text-center mb-8">
+                            <h1 className="text-2xl font-bold">Create Account</h1>
+                            <p className="text-base text-gray-600 mt-4">Ready to Play?</p>
+                        </div>
+
+                        {/* Form */}
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full max-w-md">
+                            <FormField
+                                control={form.control}
+                                name="firstName"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>First Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Your name" {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="lastName"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Last Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Your last name" {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="example@nemetschek.bg" {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit" className="w-full">
+                                Get Started
+                            </Button>
+                            
+                        </form>
+                    </div>
+
+                    {/* Right Section: Image */}
+                    <div className="w-1/2 flex items-center justify-center bg-gray-200">
+                        <img
+                            src="src/assets/images/register-pic-preview.png"
+                            alt="Example"
+                            className="max-w-full h-auto rounded-lg shadow-lg"
+                        />
+                    </div>
+                </div>
+            </Form>
+
+        </>
+    )
+}
