4037f0f2a7cfd2e24377526ac7afad4ceb60a77c
Salian Dzhihanov <saliandzhihanov@gmail.com>
2024-11-29 14:17:39 +0200

    Highest Rated Games method.
    Added method and implementation of the logic in the game service, about getting the top 3 highest rated games.

--- Boardtschek.WebAPI/Boardtschek.Services.Data/GameService.cs
+++ Boardtschek.WebAPI/Boardtschek.Services.Data/GameService.cs
@@ -1,5 +1,7 @@
 ﻿using Boardtschek.Data;
 using Boardtschek.Services.Data.Interfaces;
+using Boardtschek.WebAPI.ViewModels.Game;
+using Microsoft.EntityFrameworkCore;
 
 namespace Boardtschek.Services.Data
 {
@@ -10,6 +12,27 @@ namespace Boardtschek.Services.Data
         public GameService(BoardtschekDbContext dbContext)
         {
             this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<GameListViewModel>> GetTheThreeHighestRatedGames()
+        {
+            IEnumerable<GameListViewModel> topRatedGames = await dbContext.Games
+                .Where(g => g.Ratings.Any())
+                    .Select(g => new
+                    {
+                        Game = g,
+                        AverageRating = g.Ratings.Average(r => r.Score)
+                    })
+                .OrderByDescending(g => g.AverageRating)
+                .Take(3)
+                .Select(g => new GameListViewModel
+                {
+                    Id = g.Game.Id.ToString(),
+                    Title = g.Game.Title,
+                    ImageUrl = g.Game.ImageUrl
+                }).ToListAsync();
+
+            return topRatedGames;
         }
     }
 }
--- Boardtschek.WebAPI/Boardtschek.Services.Data/Interfaces/IGameService.cs
+++ Boardtschek.WebAPI/Boardtschek.Services.Data/Interfaces/IGameService.cs
@@ -1,6 +1,9 @@
-﻿namespace Boardtschek.Services.Data.Interfaces
+﻿using Boardtschek.WebAPI.ViewModels.Game;
+
+namespace Boardtschek.Services.Data.Interfaces
 {
     public interface IGameService
     {
+        Task<IEnumerable<GameListViewModel>> GetTheThreeHighestRatedGames();
     }
 }
