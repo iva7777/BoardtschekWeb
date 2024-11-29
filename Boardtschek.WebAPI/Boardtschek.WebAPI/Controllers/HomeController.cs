89322a97c6830497e5f37d4dd16bc7c0c2d5c0e5
Salian Dzhihanov <saliandzhihanov@gmail.com>
2024-11-29 14:41:37 +0200

    Created Home Controller

---
+++ Boardtschek.WebAPI/Boardtschek.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Boardtschek.Services.Data.Interfaces;
+using Boardtschek.WebAPI.ViewModels.Game;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Boardtschek.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IGameService gameService;
+        public HomeController(IGameService gameService)
+        {
+            this.gameService = gameService;
+        }
+    }
+}
